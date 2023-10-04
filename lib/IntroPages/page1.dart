bfef6b4dd84a8ca306d7414e5428a522c53a65c6
praful Deshmukh <prafuldeshmukh068@gmail.com>
2023-10-04 08:28:09 -0700

    Initial commit

---
+++ lib/IntroPages/page1.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 250,
+              child: Lottie.asset('assets/Animations/anim_1.json'),
+            ),
+            Text('Bitcoin  App',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color:Colors.white),)
+          ],
+        ),
+      ),
+    );
+  }
+}
